e209e600ae8c3ed2f87476a1f14e8aa4a88b3819
pratiksapkota536 <pratiksapkota536@gmail.com>
2025-04-13 00:02:38 +0545

    FinalCommitQuizPlayground

---
+++ Frontend/src/components/Playground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuestionCard from './QuestionCard'
+
+const Playground = () => {
+    return (
+        <>
+            <QuestionCard />
+        </>
+    )
+}
+
+export default Playground
