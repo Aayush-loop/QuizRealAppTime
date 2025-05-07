f58257f8d2551fd14392d1bcb363a0a865719a10
bishalsapkota001 <bishalsapkotaf@gmail.com>
2025-05-08 02:38:57 +0545

    Refactored auth.route.js for cleaner routes

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { registerUser, loginUser, logoutuser } = require('../controllers/auth.controller');
+const { registerUser, loginUser, logoutuser, getProfile } = require('../controllers/auth.controller');
 const upload = require('../middlewares/multer.middleware');
 const { checkLogin } = require('../middlewares/auth.middleware');
 
@@ -8,5 +8,6 @@ const { checkLogin } = require('../middlewares/auth.middleware');
 router.post('/register', upload.single('image'), registerUser);
 router.post('/login', loginUser);
 router.post('/logout', checkLogin, logoutuser);
+router.get('/check', checkLogin, getProfile)
 
 module.exports = router;
